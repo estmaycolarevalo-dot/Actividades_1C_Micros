4e3a3c803854302dbbf31f49ea266517a6c93518
estmaycolarevalo-dot <est.maycol.arevalo@unimilitar.edu.co>
2026-08-24 02:54:44 -0500

    Add ESP32 LED control script

---
+++ esp32_domotica.py
@@ -0,0 +1,25 @@
+import sys, select, time
+from machine import Pin
+
+led_rojo = Pin(18, Pin.OUT)
+led_verde = Pin(19, Pin.OUT)
+
+# Escucha el puerto serial (USB) sin bloquear el programa
+poll = select.poll()
+poll.register(sys.stdin, select.POLLIN)
+
+print("ESP32 listo, esperando señales...")
+
+while True:
+    if poll.poll(0):
+        linea = sys.stdin.readline().strip()
+        if linea == 'C':          # Rojo
+            led_rojo.value(1)
+            led_verde.value(0)
+        elif linea == 'M':        # Verde
+            led_verde.value(1)
+            led_rojo.value(0)
+        elif linea == 'N':        # Apagar todo
+            led_rojo.value(0)
+            led_verde.value(0)
+    time.sleep(0.05)
